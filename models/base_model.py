151d229e2988df9fe37b06e4001606f45dbd53a5
root <sebomaduna26@gmail.com>
2024-02-11 13:13:20 -0800

    No 4

--- models/base_model.py
+++ models/base_model.py
@@ -4,11 +4,18 @@ from datetime import datetime
 
 class BaseModel:
     """Defines all common attributes/methods for other classes"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Attributes of the class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                elif key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """print unique id"""
