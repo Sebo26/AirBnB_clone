bc9b1726f86fd4d31ff2d7883f05156570c7d0a3
root <sebomaduna26@gmail.com>
2024-02-11 12:32:37 -0800

    No 3

--- models/base_model.py
+++ models/base_model.py
@@ -6,16 +6,16 @@ class BaseModel:
     """defines all common attributes/methods for other classes"""
     def __init__(self):
         """Attributes of the class"""
-        self.id = uuid.uuid4()
-        self.created_at
-        self.updated_at
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
     def __str__(self):
         """print unique id"""
-        print f"[<class name>] (<self.id>) <self.__dict__>"
+        print f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """updates the public instance attribute updated_at with the current datetime"""
-        self.updated_at = datetime()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """returns dictionary, keys/values of __dict__"""
@@ -23,6 +23,4 @@ class BaseModel:
         obj_dict['__class'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
-        to_dict.to_json()
-
-    
+        return obj_dict
