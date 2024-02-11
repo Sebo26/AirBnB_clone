0688f74f16978eed607dd52184a3bb1023eae55f
root <sebomaduna26@gmail.com>
2024-02-11 07:53:18 -0800

    No 6, command interpreter

--- console.py
+++ console.py
@@ -3,14 +3,19 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """Creates entry point of the command interpreter"""
-    HBNBCommand().cmdloop()
-    HBNBCommand().emptyline()
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
         """Comaand to quit"""
         return True
-    do_EOF = do_quit
+
+    def do_EOF(self,arg):
+        """Command for EOF"""
+        return True
+
+    def emptyline(self):
+        """Used when empty line + enter are done"""
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
