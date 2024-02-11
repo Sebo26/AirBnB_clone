aa4f6ffbd9756821797cdce8c6c3a9c85d7bd843
root <sebomaduna26@gmail.com>
2024-02-11 12:48:12 -0800

    No 3

--- models/base_model.py
+++ models/base_model.py
@@ -3,15 +3,16 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    """defines all common attributes/methods for other classes"""
+    """Defines all common attributes/methods for other classes"""
     def __init__(self):
         """Attributes of the class"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
     def __str__(self):
         """print unique id"""
-        print f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        print "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """updates the public instance attribute updated_at with the current datetime"""
