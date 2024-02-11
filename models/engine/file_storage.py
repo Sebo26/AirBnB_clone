5df610b00b868656f5302913189a62868dffbd92
root <sebomaduna26@gmail.com>
2024-02-11 08:30:09 -0800

    No 5

---
+++ models/engine/file_storage.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+class FileStorage:
+    """serializes instances to a JSON file and deserializes JSON file to instances"""
+    self.__file_path
+    self.__objects
+
+    def all(self):
+        """return __objects"""
+        return __objects
+
+    def new(self, obj):
+        """Sets object with key"""
+        <obj class name>.id
+
+    def save(self):
+        """serializes __objects to the JSON file"""
+        save = JSON.dumps(__file_path)
+
+    def reload(self):
+        """deserializes JSON file if __file_path exists"""
+        if __file_path is not None:
+            reload = JSON.loads(save)
