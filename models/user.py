d0f5b28f0bdf709603632391030746085312f620
root <sebomaduna26@gmail.com>
2024-02-11 16:19:28 -0800

    No 8

--- models/user.py
+++ models/user.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """Class that inherits from BaseModel"""
+from base_model.py import BaseModel
 
 
 class User(BaseModel):
-    """Insert personal details"""
-    email = str(email)
-    password = str(password)
-    first_name = str(first_name)
-    last_name = str(last_name)
+    """Insert personal details of the user"""
+    def __init__(self, email, password, first_name, last_name):
+        super().__init__()
+        self.email = str(email)
+        self.password = str(password)
+        self.first_name = str(first_name)
+        self.last_name = str(last_name)
