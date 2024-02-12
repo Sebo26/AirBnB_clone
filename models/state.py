7364d8e5882c3c0255fa0ae94a01400a8acb497b
root <sebomaduna26@gmail.com>
2024-02-11 16:40:22 -0800

    No 9

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class that inherits from BaseModel"""
+from base_model.py import BaseModel
+
+
+class State(BaseModel):
+    """Gives name of person"""
+    def __init__(self, name)
+    """Gives name"""
+    super().__init__()
+    self.name = name
